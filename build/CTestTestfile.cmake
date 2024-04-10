6ecebdfb53431011d8d820897afa23220d0b0389
Iamborn <piyoungkoon@gmail.com>
2024-04-10 11:35:37 +0700

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lo0ker/code/projects/c++/Scholar-search
+# Build directory: /home/lo0ker/code/projects/c++/Scholar-search/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
